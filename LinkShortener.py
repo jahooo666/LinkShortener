4c090023b40d592e1cbaa8905b8ef5fd91e5c832
jahooo666 <okraskaj@ee.pw.edu.pl>
2016-01-10 13:31:14 +0100

    >Logowanie raczej działa- ale trzeba kilka rzeczy poprawić >Rejestracja podobnie >skracanie linków powinno być juz w całości, ale potrzeba testów !!- nie ma jeszcze przekierowan z linkow

--- LinkShortener.py
+++ LinkShortener.py
@@ -1,4 +1,5 @@
 # -*- coding: <utf-8> -*-
+import random
 from flask import Flask, request, redirect, render_template, session
 from werkzeug.debug import DebuggedApplication
 
@@ -12,49 +13,56 @@ app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 links = {}
 users = {'admin': 'admin'}
+users_links={}
+users_links['admin'] = {}
 
-
-@app.route(app_url + '/')
+@app.route(app_url + '/',methods=['POST','GET'])
 def index():
-    session.pop('username', None)
     if 'username' not in session:
         return redirect(app_url + "/login")
     else:
         username = session['username']
-        return render_template('login_success', username=username)
+        if request.method == 'POST':
+            long = request.form.get('long')
+            short = ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPRSTUWXYZabcdefghijklmnoprstuwxyz') for i in range(6))
+            while(short in links.keys()):
+                short = ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPRSTUWXYZabcdefghijklmnoprstuwxyz') for i in range(6))
+            print short
+            links[short]=long
+            users_links[username][short]=long
+            return render_template('shortener.html', username=username, links=users_links[username])
+        elif request.method== 'POST':
+            return render_template('shortener.html', username=username, links=users_links[username])
+
+    #session.pop('username', None)
+
+
 
 
 @app.route('/mojelinki', methods=['GET', 'POST'])
 def moje_linki():
     request.get_data()
-    return render_template('moje_linki.html', links=linki_usera)
+    return render_template('moje_linki.html', links=links)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'username' in session:
-        session.pop('username', None)
-
     if request.method == 'GET':
         return render_template('login.html')
-
-    if request.method == 'POST':
+    elif request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-
-        global links
-        links[username] = {}
+        # global links
+        # links[username] = {}
         # tworzymy uzytkownikowi slownik jego linkow
-
-        if username not in session:
-            if username in users:
-                if users[username] == password:
-                    session[username] = username
-                    return render_template('login_succes.html', username=username)
-                else:
-                    return render_template('login_failure.html',username=username, reason= 'pass')
+        if username in users:
+            if users[username] == password:
+                session['username'] = username
+                return render_template('login_succes.html',username= username)
             else:
-                return render_template('login_failure.html',username= username, reason = 'username')
+                return render_template('login_failure.html',username=username, reason='pass')
+        else:
+            return render_template('login_failure.html',username=username, reason='username')
 
 
 
@@ -66,15 +74,14 @@ def register():
         password = request.form.get('password')
 
         if username not in users:
-            #rejestracja
-            users[username]= password
-            #logowanie
+            # rejestracja
+            users[username] = password
+            users_links[username]={}
+            # logowanie
             session[username] = username
             render_template('register_succes.html', username=username)
         else:
             return render_template('register_failure.html', username=username)
-
-
 
     return render_template('register.html')
 
